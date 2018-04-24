7c33caf5d8a19636050ac8860ee07b686d5e34e0
wetru <37796913+wetru@users.noreply.github.com>
2018-04-24 15:23:46 +0300

    Add files via upload

---
+++ karpenko_Olena/Новая папка/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	  <!-- Важная техническая информация для браузера -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Информация об Авторе и описание проекта -->
+		<meta name="description" content="Веб-разработчица Алена Карпенко">
+		<meta name="auyhor" content="Алена Карпенко">
+
+		<!-- Иконка, которая будет на вкладке в браузере -->
+		<link rel="icon" href="img/ico.png">
+		<!-- Заголовок на вкладке в браузере -->
+		<title>Алена Карпенко разработка сайтов</title>
+		<!-- Шривы от гугл -->
+		<!--<link rel="stylesheet" href="fonts/Roboto/._Roboto-Regular.ttf">-->
+		
+		<!-- Подключаем наш файл со стилями, который находится в папке css-->
+		<link rel="stylesheet" href="css/animated.css">
+		<link rel="stylesheet" href="css/bootstrap-grid.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+
+</head>
+<body>
+ <!-- Тут будет модержимое будущего сайта -->
+<header>
+	<div class="conteiner">
+		<div class="row">
+			<div class="col-lg-7">
+				<img class="header-img animated fadeInLeftBig" src="img/0.png" alt="monitor">
+			</div>
+			<div class="col-5">
+					<p class="header-pretitle animated fadeInRightBig">Веб-разработчик</p>
+					<h1 class="header-title animated fadeInRightBig">Алена Карпенко</h1>
+					<p class="Subt animated fadeInRightBig">Верстка на HTML/CSS<br> Разработка сайтов под ключ</p>
+					<div class="header-button-blok animated fadeInRightBig">
+						<a href=" https://vk.com/wetru" class="header-button">Связаться
+					</a>
+					<small>*Это бесплатно</small>
+					</div>
+			</div>
+		</div>
+	</div>
+</header>
+<section class="portfolio">
+	<div class="conteiner">
+		<div class="portfolio-block">
+		<h2 class="portfolio-title">Портфолио</h2>
+<p class="portfolio-sub1"> Мои работы</p>
+		<div class="row">
+			<div class="col-sd-6 col-lg-offset-2 col-sm-6 col-lg-offset-1 col-xs-3 col-lg-offset-0">
+				<div class="portfolio-col1">
+					<img src="img/1.png" alt="3" >
+				<h3>внутренний университет «Газпром нефти»</h3>
+				<p class="portfolio-sub1">
+					У «Газпром нефти» есть внутренний Корпоративный университет для сотрудников. Это настоящий университет, где сотрудники проходят обучение у внутренних тренеров компании или у специально приглашенных лекторов и экспертов....
+				</p>
+				<a href="#"><u>Смотреть работу</u></a>
+				</div>
+				
+			</div>
+			<!-- /.col-6 -->
+			<div class="col-sd-6 col-lg-offset-2 col-sm-6 col-lg-offset-1 col-xs-3 col-lg-offset-0">
+			<div class="portfolio-col2">
+				<img src="img/2.png" alt="4">
+			<h3>Сайт платежной системы «Мир»	</h3>
+			<p class="portfolio-sub2">
+				Обновленная версия сайта знакомит посетителей с компаниями, уже принимающими карты к оплате, и приглашает всех остальных присоединиться к проекту...
+			</p>
+			<a href="#"><u>Смотреть работу</u></a>
+			</div>
+			</div>
+			
+			<!-- /.col-6 -->
+		</div>
+		</div>
+		</div>
+
+</section>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-2">
+				<h4 class="footer-auther">Алена Карпенко</h4>
+				<p class="footer-text"> Разработка сайтов под ключ</p>
+			</div>
+			<div class="col-md-4 col-sm-2 ">
+				<div class="social-block animated fadeInUp">
+					<p> Подпишитесь:</p>
+					<a href="#"><img src="img/youtube.png" alt="youtube"></a>
+					<a href="#"><img src="img/fasebook.png" alt="facebook"></a>
+					<a href="#"><img src="img/telegramm.png" alt="telegramm"></a>
+					<a href="#"><img src="img/vk.png" alt="vk"></a>
+			</div>
+		</div>
+
+			<div class="col-md-4 col-sm-2 ">
+				<div class="footor-cta-block">
+					<p class="footer-feedback">Напишите мне:</p>
+				<a href=" https://vk.com/wetru" class="footer-button"  >Написать</a>
+				</div>
+			</div>
+		
+	</div>
+</footer>
+</body>
+</html>
